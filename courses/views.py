d74b7ea4120660faa3f7f24e5ce638df9d4a4cbd
ycoodr <ycoodr@gmail.com>
2022-08-12 15:06:10 +0300

    template

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Course
+
+def index(request):
+    courses = Course.objects
+    return render(request, 'courses/index.html', {'courses': courses})
 
 # Create your views here.
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'courses.app.coursesConfig',
+    'courses.apps.CoursesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
