d34d88f309c3f7b0d4d38fa7c96e2857984c7e70
ycoodr <ycoodr@gmail.com>
2022-08-12 17:21:00 +0300

    end

--- courses/views.py
+++ courses/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Course
 
 def index(request):
     courses = Course.objects
     return render(request, 'courses/index.html', {'courses': courses})
+
+def detail(request, course_id):
+    course_detail = get_object_or_404(Course, pk = course_id)
+    return render(request, 'courses/detail.html', {'course': course_detail})
 
 # Create your views here.
